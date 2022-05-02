b217e866e14a23cd2fe9ec456e657088d710bfbf
nanjizal <nanjizal@users.noreply.github.com>
2022-05-02 01:49:10 +0100

    Create EgocentricCoordinates.hx

---
+++ src/contacthx/direction/EgocentricCoordinates.hx
@@ -0,0 +1,11 @@
+package contacthx.direction;
+
+enum abstract EgocentricCoordinates( String ) from String {
+    var NONE     = "0";
+    var LEFT     = "left";
+    var RIGHT    = "right";
+    var UP       = "up";
+    var DOWN     = "down";
+    var FORWARD  = "forward";
+    var BACKWARD = "backward";
+}
