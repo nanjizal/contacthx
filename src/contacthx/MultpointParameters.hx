5204835bbf8658e2e4ca8e17b4c2e94d5ac66528
nanjizal <nanjizal@users.noreply.github.com>
2022-05-02 01:33:42 +0100

    Create MultpointParameters.hx

---
+++ src/contacthx/MultpointParameters.hx
@@ -0,0 +1,26 @@
+package contacthx;
+import contacthx.geom.VectorStartEnd;
+
+@:StructInit
+class MultipointParameters {
+    public var centerMovement:         Float;
+    public var centerMovementVector:   VectorStartEnd;
+    public var distanceChange:         Float;
+    public var relativeDistanceChange: Float;
+    public var rotationAngle:          Float;
+    public var vectorAngle:            Float;
+    public function new( centerMovement:         Float
+                       , centerMovementVector:   VectorStartEnd
+                       , distanceChange:         Float
+                       , relativeDistanceChange: Float
+                       , rotationAngle:          Float
+                       , vectorAngle:            Float
+                        ){
+        this.centerMovement         = centerMovement;
+        this.centerMovementVector   = centerMovementVector;
+        this.distanceChange         = distanceChange;
+        this.relativeDistanceChange = relativeDistanceChange;
+        this.rotationAngle          = rotationAngle;
+        this.vectorAngle            = vectorAngle;
+    }
+}
